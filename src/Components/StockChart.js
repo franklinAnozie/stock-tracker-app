262ec42e97fa1aed407c9e1cf342223d891916b0
franklinAnozie <cfanozie@gmail.com>
2023-02-06 17:16:47 +0100

    Fixed security issue

--- src/Components/StockChart.js
+++ src/Components/StockChart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
-export const StockChart = () => {
+export const StockChart = ({chartData, symbol}) => {
+    const {day, week, year} = chartData
     return <div>Stock Chart</div>
 }
